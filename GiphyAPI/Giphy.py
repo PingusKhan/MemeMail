52eee4d6f5cdb25a9834b050388db94abb371698
Olga Shigapova <oshigapo@uci.edu>
2019-02-16 02:49:00 -0800

    returns urls for generated giphy searches

--- GiphyAPI/Giphy.py
+++ GiphyAPI/Giphy.py
@@ -1,13 +1,17 @@
 import json
 import urllib.request
 
+#json format of GIF Object
+data = json.loads(urllib.request.urlopen("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=Oroe1G0gBq2z6n2lD0fA5r2UC3SNA9u6&limit=5").read())
 
-#contents = urllib.request.urlopen("http://api.giphy.com/v1/gifs/search?q=quora&api_key=dc6zaTOxFJmzC").read()
-#jsonContent = json.loads(contents)
-
-#print(jsonContent['data'][0]['images']['fixed_height']['url'])
-
-data = json.loads(urllib.request.urlopen("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=Oroe1G0gBq2z6n2lD0fA5r2UC3SNA9u6&limit=1").read())
+#formatted to look nice
 gifs = json.dumps(data, sort_keys=True, indent=4)
-out = data['data']
-print(data)
+
+#adding all the generated urls to a list and printing
+urls = list()
+for i in range(len(data['data'])):
+    urls.append(data['data'][i]['images']['fixed_height']['url'])
+print(urls)
+
+out1 = data['data'][0]['images']['fixed_height']['url']
+
