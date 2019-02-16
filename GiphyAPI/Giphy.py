97e85f702a6de5df2895aa8db965c46c7ff33ead
Olga Shigapova <oshigapo@uci.edu>
2019-02-16 02:26:58 -0800

    trying to access url from GIF object

--- GiphyAPI/Giphy.py
+++ GiphyAPI/Giphy.py
@@ -1,2 +1,13 @@
-print("Hello World!")
-print("Olga is the best!")
+import json
+import urllib.request
+
+
+#contents = urllib.request.urlopen("http://api.giphy.com/v1/gifs/search?q=quora&api_key=dc6zaTOxFJmzC").read()
+#jsonContent = json.loads(contents)
+
+#print(jsonContent['data'][0]['images']['fixed_height']['url'])
+
+data = json.loads(urllib.request.urlopen("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=Oroe1G0gBq2z6n2lD0fA5r2UC3SNA9u6&limit=1").read())
+gifs = json.dumps(data, sort_keys=True, indent=4)
+out = data['data']
+print(data)
