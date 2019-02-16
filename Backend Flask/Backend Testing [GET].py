30b07deae3a1c3e0d3e51655ffc330445b13bda6
Khiem Dinh <pinguskhan@gmail.com>
2019-02-16 13:58:09 -0800

    some new functions...

--- Backend Flask/Backend Testing [GET].py
+++ Backend Flask/Backend Testing [GET].py
@@ -1,1 +1,12 @@
-print("testing")
+import sendgrid
+from sendgrid.helpers.mail import *
+
+def add_subscription():
+    pass
+
+def remove_subscription():
+    pass
+
+def send_memes_to_subscribers():
+    pass
+
