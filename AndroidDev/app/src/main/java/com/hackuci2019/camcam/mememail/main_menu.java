cef747ad3d1e1056fe2072da88244d01757aae2b
CamCam <CamCam@Progenitor>
2019-02-16 19:49:03 -0800

    Working on the Android App.

---
+++ AndroidDev/app/src/main/java/com/hackuci2019/camcam/mememail/main_menu.java
@@ -0,0 +1,19 @@
+package com.hackuci2019.camcam.mememail;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class main_menu extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu);
+    }
+    public void onSubscribe(View view){
+        Intent goToNewSubscribe = new Intent(this, sub_menu.class);
+        startActivity(goToNewSubscribe);
+    }
+
+}
---
+++ AndroidDev/app/src/main/java/com/hackuci2019/camcam/mememail/sub_menu.java
@@ -0,0 +1,13 @@
+package com.hackuci2019.camcam.mememail;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class sub_menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sub_menu);
+    }
+}
