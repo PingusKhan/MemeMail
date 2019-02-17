193ea4e3125aea85bd1e0aac7779d00b5df83c22
CamCam <CamCam@Progenitor>
2019-02-17 06:31:04 -0800

    Added Backend POST functionality; UNTESTED [CLEANED! v.2]

--- AndroidDev/app/src/main/java/com/hackuci2019/camcam/mememail/sub_menu.java
+++ AndroidDev/app/src/main/java/com/hackuci2019/camcam/mememail/sub_menu.java
@@ -1,11 +1,9 @@
 package com.hackuci2019.camcam.mememail;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -14,19 +12,12 @@ import android.widget.ListView;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import okhttp3.Call;
 import okhttp3.OkHttpClient;
-import okhttp3.Request;
 import okhttp3.RequestBody;
-import okhttp3.Response;
 
 import okhttp3.MediaType;
 
