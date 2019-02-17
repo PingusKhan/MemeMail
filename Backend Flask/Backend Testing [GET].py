6b086d6da3b32bf3cc121429e0c022c8bac3b0cb
PingusKhan <pinguskhan@gmail.com>
2019-02-16 17:48:42 -0800

    probably missing a method or two, but this should be good enough for now i think...

--- Backend Flask/Backend Testing [GET].py
+++ Backend Flask/Backend Testing [GET].py
@@ -4,6 +4,7 @@ from collections import namedtuple
 from datetime import *
 
 APIKEY = 'MY_API_KEY'
+# ^^^ cant display api key cuz it gets my accound suspended lmao
 sg = sendgrid.SendGridAPIClient(APIKEY)
 
 Subscriptions = {}
@@ -13,6 +14,9 @@ Subscriber = namedtuple('Subscription', ['Genre', 'Interval',
 
 def add_new_subscription(recipient: str, genre: str, interval: timedelta, 
                         sender: str) -> None:
+    # Takes in the input of recipient of the subscription, genre of memes, interval in which they receive
+    # the memes, and who is the sender; Organizes multiple subscriptions as a dictionary of Subscriber
+    # namedtuples, with the recipient as the key
     Subscriptions[recipient] = Subscriber(Genre = genre,
                                         Interval = interval,
                                         Sender = sender,
@@ -20,39 +24,30 @@ def add_new_subscription(recipient: str, genre: str, interval: timedelta,
 
 
 def is_it_meme_time(subscriptions: dict) -> None:
+    # Checks if it is time for memes to be sent to the recipient based on the interval that was selected
+    # Note: this method will probably be run hourly in the app to constantly check all subscriptions 
     for subscriber in subscriptions:
-        print(subscriber)
         meme_time = subscriptions[subscriber].Date + subscriptions[subscriber].Interval
-        print(meme_time)
-        print(meme_time < datetime.utcnow())
         if meme_time <= datetime.utcnow():  
             it_be_meme_time(subscriptions[subscriber], subscriber)
     
 
 def it_be_meme_time(subscriber: Subscriber, recipient: str) -> None:
+    # The method that sends the actual meme! Uses the Sendgrid API
     sg = sendgrid.SendGridAPIClient(APIKEY)
 
     from_email = Email(subscriber.Sender)
     to_email = Email(recipient)
     subject = "Meme Mail brought straight to YOU by Team TBD"
+    # vvvv this line is the one responsible for the body of the meme mail!!!
     content = Content("text/html", "<h1>You like memes?</h1><img src={} alt=dank_meme.jpg style=width:auto;height:auto></img>".format(get_meme(subscriber.Genre)))
 
     mail = Mail(from_email, subject, to_email, content)
     response = sg.client.mail.send.post(request_body=mail.get())
 
-    print(response.status_code)
-    print(response.body)
-    print(response.headers)
-
 
 def get_meme(genre: str) -> str:
+    # Gets the url of the meme to display within the email
+    # Note: atm it returns only a singular url because it needs Giphy implementation...
      return "https://i.ytimg.com/vi/p-Ww_oTfLcA/hqdefault.jpg"
 
-
-if __name__ == "__main__":
-    Subscriptions["pinguskhan@gmail.com"] = Subscriber(Genre = "genre",
-                                        Interval = timedelta(hours=6),
-                                        Sender = "kgdinh@uci.edu",
-                                        Date = datetime.today())
-    print(datetime.today())
-    is_it_meme_time(Subscriptions)
