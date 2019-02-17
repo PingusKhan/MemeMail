4a77622a565726184f47c9f2353f161168e973ef
CamCam <CamCam@Progenitor>
2019-02-16 20:59:55 -0800

    Completed UI/UX for Android Subscribe menu.

--- AndroidDev/app/src/main/java/com/hackuci2019/camcam/mememail/sub_menu.java
+++ AndroidDev/app/src/main/java/com/hackuci2019/camcam/mememail/sub_menu.java
@@ -2,6 +2,7 @@ package com.hackuci2019.camcam.mememail;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class sub_menu extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class sub_menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sub_menu);
+    }
+    protected void addGenre(View view) {
+        String genre_to_add = findViewById(R.id.genre_input_editText).toString();
+
     }
 }
