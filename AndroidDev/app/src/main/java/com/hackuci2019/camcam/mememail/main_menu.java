0f62ab85aec5a170c1a1c8e0156622d74fa2b4c9
CamCam <CamCam@Progenitor>
2019-02-17 02:40:51 -0800

    Working Android frontend.

--- AndroidDev/app/src/main/java/com/hackuci2019/camcam/mememail/main_menu.java
+++ AndroidDev/app/src/main/java/com/hackuci2019/camcam/mememail/main_menu.java
@@ -4,18 +4,47 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 public class main_menu extends AppCompatActivity {
-
-
-
-
+    private ListView listView_subs;
+    public JSONObject subscriber_json;
+    ArrayList<String> arrayList_subscribers;
+    ArrayAdapter<String> arrayAdapter_subscribers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu);
+       listView_subs = findViewById(R.id.subscribers_listView);
+
+
+        Map<String, Map> data = DataHolder.getInstance().getData();
+        if (!(data == null)) {
+            arrayList_subscribers = new ArrayList<>();
+            arrayAdapter_subscribers = new ArrayAdapter<>(this,
+                    android.R.layout.simple_list_item_1);
+            listView_subs.setAdapter(arrayAdapter_subscribers);
+
+            Iterator<String> email_keys = data.keySet().iterator();
+            while (email_keys.hasNext()) {
+                Map<String, Object> sub_data = data.get(email_keys.next());
+                arrayAdapter_subscribers.add(sub_data.get("email_to") + "\nInterval: "
+                        + sub_data.get("hours") + " hrs, " + sub_data.get("minutes") + " minutes\n"
+                        + "Genres: " + sub_data.get("genres").toString());
+            }
+        }
     }
+
     public void onSubscribe(View view){
         Intent goToNewSubscribe = new Intent(this, sub_menu.class);
         startActivity(goToNewSubscribe);
--- AndroidDev/app/src/main/java/com/hackuci2019/camcam/mememail/sub_menu.java
+++ AndroidDev/app/src/main/java/com/hackuci2019/camcam/mememail/sub_menu.java
@@ -9,10 +9,16 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class sub_menu extends AppCompatActivity {
+    private Map<String, Map> stored_subs;
+
     private ListView listView_genres;
     private ArrayList<String> arrayList_genres;
     private ArrayAdapter<String> arrayAdapter_genres;
@@ -21,6 +27,7 @@ public class sub_menu extends AppCompatActivity {
     private EditText email_input_to_editText;
     private EditText interval_hours_editText;
     private EditText interval_mins_editText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,25 +59,24 @@ public class sub_menu extends AppCompatActivity {
         }
     }
 
-    public void subscribeMenu(MenuItem menuItem) {
+    public void subscribeMenu(View view) {
         String email_to = email_input_to_editText.getText().toString();
         String email_from = email_input_from_editText.getText().toString();
         //arrayList_genres
         String hours = interval_hours_editText.getText().toString();
         String minutes = interval_mins_editText.getText().toString();
-
-        clear();
         Intent goToMainMenu = new Intent(this, main_menu.class);
+        Map<String, Object> new_sub = new HashMap<>();
+
+        new_sub.put("email_to", email_to);
+        new_sub.put("email_from", email_from);
+        new_sub.put("hours", hours);
+        new_sub.put("minutes", minutes);
+        new_sub.put("genres", arrayList_genres);
+
+        DataHolder.getInstance().setData(new_sub);
+
         startActivity(goToMainMenu);
     }
 
-    public void clear() {
-        genre_input_editText.setText("");
-        email_input_from_editText.setText("");
-        email_input_to_editText.setText("");
-        interval_hours_editText.setText("");
-        interval_mins_editText.setText("");
-        arrayList_genres.clear();
-
-    }
 }
